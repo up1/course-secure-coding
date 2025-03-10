bacb3664092b25bc0e183e8e34a4c4e45c77e6d4
up1 <somkiat.p@gmail.com>
2025-03-10 22:01:26 +0700

    Remoce unuse code

--- workshop/demo-dotnet/01-api/Controllers/Auth.cs
+++ workshop/demo-dotnet/01-api/Controllers/Auth.cs
@@ -1,7 +1,6 @@
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.IdentityModel.Tokens;
 
