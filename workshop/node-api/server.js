1e5f9f79157aa4451a602be5038e7b1a580331da
up1 <somkiat.p@gmail.com>
2025-07-14 23:23:52 +0700

    Fix

--- workshop/node-api/server.js
+++ workshop/node-api/server.js
@@ -3,11 +3,10 @@ const swaggerUi = require('swagger-ui-express')
 const swaggerFile = require('./swagger_output.json')
 const person = require('./routers/personRouter')
 
-const router = express.Router();
 const app = express();
 
 app.use(express.json());
-app.use('/person', person.personRouter)
+app.use('/', person.personRouter)
 app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerFile))
 
 app.listen(3000, () => {
