bd769ab9fad166ed43420280dec0a9bf968887e3
up1 <somkiat.p@gmail.com>
2025-07-15 08:15:44 +0700

    Reset code

--- workshop/node-api/server.js
+++ workshop/node-api/server.js
@@ -7,28 +7,28 @@ const cors = require('cors');
 
 const app = express();
 
-app.use(helmet());
-app.use(express.json());
-app.use(cors());
+// app.use(helmet());
+// app.use(express.json());
+// app.use(cors());
 
 // Disable OPTIONS method globally
-app.use((req, res, next) => {
-  if (req.method === 'OPTIONS') {
-    return res.status(405).send({ error: "Method Not Allowed" });
-  }
-  next();
-});
+// app.use((req, res, next) => {
+//   if (req.method === 'OPTIONS') {
+//     return res.status(405).send({ error: "Method Not Allowed" });
+//   }
+//   next();
+// });
 
 app.use("/", person.personRouter);
 app.use("/doc", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 // Global error handler with method not allowed
-app.use((req, res, next) => {
-  res.status(405).json({
-    error: "Method Not Allowed",
-    message: `The method ${req.method} is not allowed for the requested URL ${req.originalUrl}`,
-  });
-});
+// app.use((req, res, next) => {
+//   res.status(405).json({
+//     error: "Method Not Allowed",
+//     message: `The method ${req.method} is not allowed for the requested URL ${req.originalUrl}`,
+//   });
+// });
 
 app.listen(3000, () => {
   console.log(`Running on 3000`);
